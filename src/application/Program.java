a1e9b2945ebe6d6a1773cee9bec44cd25076197e
Marcelo Pirro <marcelopirro98@gmail.com>
2023-06-14 19:07:19 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -35,5 +35,13 @@ public class Program {
         Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
+
+
+        System.out.println("\n=== TEST 5: seller update ===");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha Wayne");
+        sellerDao.update(seller);
+        System.out.println("Update completed");
+        
     }
 }
