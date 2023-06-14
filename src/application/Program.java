fa1b650ec2b441c66864327933cd206c8a344f25
Marcelo Pirro <marcelopirro98@gmail.com>
2023-06-14 17:26:45 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,15 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
     public static void main(String[] args) {
         Department obj = new Department(1, "Books");
-        System.out.println(obj);
+
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, obj);
+        System.out.println(seller);
     }
 }
--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 
 public class Department implements Serializable{
     
-    private static final long serialVersionUID = 1;
+    private static final long serialVersionUID = 1L;
     private Integer id;
     private String name;
 
