e703c573bebc22d8b5d66d11832fabbc58af85b5
Marcelo Pirro <marcelopirro98@gmail.com>
2023-06-14 19:32:28 -0300

    DepartmentDaoJDBC implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,6 +1,7 @@
 package model.dao;
 
 import db.DB;
+import model.dao.impl.DepartmentDaoJDBC;
 import model.dao.impl.SellerDaoJDBC;
 
 //Static Operations for Instantiating DAO
@@ -10,4 +11,8 @@ public class DaoFactory {
     public static SellerDao createSellerDao(){
         return new SellerDaoJDBC(DB.getConnection());
     }
+
+    public static DepartmentDao createDepartmentDao() {
+		return new DepartmentDaoJDBC(DB.getConnection());
+	}
 }
